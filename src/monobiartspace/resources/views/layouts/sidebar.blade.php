2d009af61169371eefefcda16db7c7e8496d3f92
furqonaugust17 <furqonaugustseventeenth@gmail.com>
2025-04-09 11:25:48 +0700

    update: update project resource

---
+++ src/monobiartspace/resources/views/layouts/sidebar.blade.php
@@ -0,0 +1,30 @@
+<div class="deznav">
+    <div class="deznav-scroll">
+        <div class="main-profile">
+            <div class="image-bx">
+                <img src="images/Untitled-1.jpg" alt="">
+                <a href="javascript:void(0);"><i class="fa fa-cog" aria-hidden="true"></i></a>
+            </div>
+            <h5 class="name"><span class="font-w400">Hello,</span> Marquez</h5>
+            <p class="email">marquezzzz@mail.com</p>
+        </div>
+        <ul class="metismenu" id="menu">
+            <li class="nav-label first">Main Menu</li>
+            <li><a class="has-arrow ai-icon" href="javascript:void()" aria-expanded="false">
+                    <i class="flaticon-144-layout"></i>
+                    <span class="nav-text">Dashboard</span>
+                </a>
+                <ul aria-expanded="false">
+                    <li><a href="index.html">Dashboard Light</a></li>
+                    <li><a href="index-2.html">Dashboard Dark</a></li>
+                    <li><a href="my-wallets.html">Wallet</a></li>
+                    <li><a href="tranasactions.html">Transactions</a></li>
+                    <li><a href="coin-details.html">Coin Details</a></li>
+                    <li><a href="portofolio.html">Portofolio</a></li>
+                    <li><a href="market-capital.html">Market Capital</a></li>
+                </ul>
+
+            </li>
+        </ul>
+    </div>
+</div>
