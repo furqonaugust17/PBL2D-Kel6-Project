b2c67f11cab42cb05936ddaa0a6d66ad7765949b
Abdhusyukra <abdhusyukra02@gmail.com>
2025-05-04 01:33:05 +0700

    fix: delete old CRUD

--- src/monobiartspace/resources/views/backend/kelas/edit.blade.php
+++
@@ -1,8 +0,0 @@
-@section('script')
-    <script src="{{ asset('plugins/vendor/peity/jquery.peity.min.js') }}"></script>
-    <script src="{{ asset('plugins/vendor/apexchart/apexchart.js') }}"></script>
-    <script src="{{ asset('plugins/js/dashboard/dashboard-1.js') }}"></script>
-@endsection
-<x-app-layout>
-    <x-slot:title>Edit Kelas</x-slot:title>
-</x-app-layout>
