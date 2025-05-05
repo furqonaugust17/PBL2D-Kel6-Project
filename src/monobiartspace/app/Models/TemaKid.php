97b7816983e7d9391847adf982af726ad3830ded
furqonaugust17 <furqonaugustseventeenth@gmail.com>
2025-05-05 20:40:23 +0700

    feat: create model and migration for kids tema and detail tema

---
+++ src/monobiartspace/app/Models/TemaKid.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
+use Illuminate\Database\Eloquent\Relations\HasMany;
+use Illuminate\Database\Eloquent\SoftDeletes;
+
+class TemaKid extends Model
+{
+    use HasFactory, SoftDeletes;
+    protected $fillable = ['nama', 'waktu', 'kid_id'];
+    protected $with = ['kid', 'detailTema'];
+
+    public function kid(): BelongsTo
+    {
+        return $this->belongsTo(Kid::class);
+    }
+
+    public function detailTema(): HasMany
+    {
+        return $this->hasMany(DetailTemaKid::class);
+    }
+}
