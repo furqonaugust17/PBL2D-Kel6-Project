b298e449f1fdf4b50347d5193b8300efb35f0170
furqonaugust17 <furqonaugustseventeenth@gmail.com>
2025-05-05 20:37:06 +0700

    feat: create relationship for kids kategori and tema

--- src/monobiartspace/app/Models/Kid.php
+++ src/monobiartspace/app/Models/Kid.php
@@ -4,10 +4,21 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\HasMany;
 use Illuminate\Database\Eloquent\SoftDeletes;
 
 class Kid extends Model
 {
     use HasFactory, SoftDeletes;
     protected $fillable = ['nama'];
+
+    public function kategori(): HasMany
+    {
+        return $this->hasMany(KategoriKid::class);
+    }
+
+    public function tema(): HasMany
+    {
+        return $this->hasMany(TemaKid::class);
+    }
 }
