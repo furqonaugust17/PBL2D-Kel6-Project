b2929f91d639c4b27f04345e42f5f85fdf5e5a54
furqonaugust17 <furqonaugustseventeenth@gmail.com>
2025-05-05 20:42:01 +0700

    feat: create model and migration for kegiatan artspace

---
+++ src/monobiartspace/app/Models/KegiatanArtSpace.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
+use Illuminate\Database\Eloquent\SoftDeletes;
+
+class KegiatanArtSpace extends Model
+{
+    use HasFactory, SoftDeletes;
+    protected $fillable = ['nama', 'harga', 'artspace_id'];
+    protected $with = ['artspace'];
+
+    public function artspace(): BelongsTo
+    {
+        return $this->belongsTo(ArtSpace::class);
+    }
+}
