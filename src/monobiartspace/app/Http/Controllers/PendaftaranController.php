6f6f82794e29531f4a781ee4e267314097dd046b
furqonaugust17 <furqonaugustseventeenth@gmail.com>
2025-05-19 11:34:29 +0700

    fix: change midtrans key

--- src/monobiartspace/app/Http/Controllers/PendaftaranController.php
+++ src/monobiartspace/app/Http/Controllers/PendaftaranController.php
@@ -59,13 +59,13 @@ class PendaftaranController extends Controller
 
             // Redirect atau return Midtrans Snap Token
             // Set your Merchant Server Key
-            \Midtrans\Config::$serverKey = 'SB-Mid-server-Quz-xm_5OB9vKo4oRAGr5TM7';
+            \Midtrans\Config::$serverKey = env('MIDTRANS_SERVER_KEY');
             // Set to Development/Sandbox Environment (default). Set to true for Production Environment (accept real transaction).
-            \Midtrans\Config::$isProduction = false;
+            \Midtrans\Config::$isProduction = env('MIDTRANS_IS_PRODUCTION');
             // Set sanitization on (default)
-            \Midtrans\Config::$isSanitized = true;
+            \Midtrans\Config::$isSanitized = env('MIDTRANS_IS_SANITIZED');
             // Set 3DS transaction for credit card to true
-            \Midtrans\Config::$is3ds = true;
+            \Midtrans\Config::$is3ds = env('MIDTRANS_IS_3DS');
 
             $kegiatans = KegiatanArtSpace::all()->keyBy('id');
             $participants = collect($request->participants);
