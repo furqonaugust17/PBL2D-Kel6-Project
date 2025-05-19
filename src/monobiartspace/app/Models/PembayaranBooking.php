39f34c4bd390adc964aed9fc33b6ec532228314e
furqonaugust17 <furqonaugustseventeenth@gmail.com>
2025-05-19 15:52:57 +0700

    feat: implementation midtrans payments gateway

---
+++ src/monobiartspace/app/Models/PembayaranBooking.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\SoftDeletes;
+
+class PembayaranBooking extends Model
+{
+    use HasFactory, SoftDeletes;
+    protected $fillable = ['order_id', 'pendaftaran_id', 'payment_date', 'amount',  'payment_method', 'status', 'snap_token'];
+}
