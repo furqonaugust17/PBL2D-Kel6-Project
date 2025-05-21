091dc544aea8b35ccfde1bde60af536678c9d99c
furqonaugust17 <furqonaugustseventeenth@gmail.com>
2025-05-21 22:35:43 +0700

    feat: create jadwal kids endpoint

--- src/monobiartspace/routes/api.php
+++ src/monobiartspace/routes/api.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\JadwalKidController;
 use App\Http\Controllers\KategoriKidController;
 use App\Http\Controllers\PaymentController;
 use App\Http\Controllers\TemaKidController;
@@ -12,6 +13,7 @@ use Illuminate\Support\Facades\Route;
 Route::prefix('kids')->group(function () {
     Route::get('/kategori/{id}', [KategoriKidController::class, 'getData'])->name('kategori.getdata');
     Route::get('/tema/{id}', [TemaKidController::class, 'getData'])->name('tema.getdata');
+    Route::get('/jadwal/{id}', [JadwalKidController::class, 'getData'])->name('jadwal.getdata');
 });
 
 Route::post('payment/notification', [PaymentController::class, 'callback']);
