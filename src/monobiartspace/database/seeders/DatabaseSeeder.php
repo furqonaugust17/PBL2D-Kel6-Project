ff56d41c44d2f91cdb0962b29e1eb993c482ccd4
furqonaugust17 <furqonaugustseventeenth@gmail.com>
2025-05-23 11:15:31 +0700

    test: update KaryawanSeeder

--- src/monobiartspace/database/seeders/DatabaseSeeder.php
+++ src/monobiartspace/database/seeders/DatabaseSeeder.php
@@ -16,12 +16,15 @@ class DatabaseSeeder extends Seeder
     {
         // User::factory(10)->create();
 
-        $this->call([KaryawanSeeder::class]);
-        // User::factory()->create([
-        //     'name' => 'superadmin',
-        //     'email' => 'superadmin@gmail.com',
-        //     'password' => Hash::make('password'),
-        //     'email_verified_at' => now()
-        // ]);
+        $this->call([RolesAndPermissionsSeeder::class, KaryawanSeeder::class]);
+
+        $superadmin = User::create([
+            'name' => 'superadmin',
+            'email' => 'superadmin@gmail.com',
+            'password' => Hash::make('password'),
+            'email_verified_at' => now()
+        ]);
+
+        $superadmin->assignRole('supervisor');
     }
 }
--- src/monobiartspace/database/seeders/KaryawanSeeder.php
+++ src/monobiartspace/database/seeders/KaryawanSeeder.php
@@ -3,6 +3,7 @@
 namespace Database\Seeders;
 
 use App\Models\Karyawan;
+use App\Models\User;
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
 
@@ -13,6 +14,7 @@ class KaryawanSeeder extends Seeder
      */
     public function run(): void
     {
-        Karyawan::factory(100)->create();
+        User::factory(20)->create();
+        Karyawan::factory(20)->recycle([User::all()])->create();
     }
 }
