caf3eb3b63b707f925cc3d744f5ada7e45236845
furqonaugust17 <furqonaugustseventeenth@gmail.com>
2025-05-25 10:44:24 +0700

    feat: change midtrans configuration

--- src/monobiartspace/app/Http/Controllers/PaymentController.php
+++ src/monobiartspace/app/Http/Controllers/PaymentController.php
@@ -7,10 +7,17 @@ use Illuminate\Http\Request;
 
 class PaymentController extends Controller
 {
+
+    public function __construct()
+    {
+        \Midtrans\Config::$serverKey = env('MIDTRANS_SERVER_KEY');
+        \Midtrans\Config::$isProduction = env('MIDTRANS_IS_PRODUCTION');
+        \Midtrans\Config::$isSanitized = env('MIDTRANS_IS_SANITIZED');
+        \Midtrans\Config::$is3ds = env('MIDTRANS_IS_3DS');
+    }
+
     public function callback(Request $request)
     {
-        \Midtrans\Config::$isProduction = false;
-        \Midtrans\Config::$serverKey = env('MIDTRANS_SERVER_KEY');
         $notif = new \Midtrans\Notification();
         $transaction = $notif->transaction_status;
         $type = $notif->payment_type;
