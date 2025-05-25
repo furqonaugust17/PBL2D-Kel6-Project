2699ee879268d6e4818130b7e57ad328b00f1f0d
furqonaugust17 <furqonaugustseventeenth@gmail.com>
2025-05-25 10:47:40 +0700

    feat: implementation midtrans

--- src/monobiartspace/resources/views/frontend/booking/kid.blade.php
+++ src/monobiartspace/resources/views/frontend/booking/kid.blade.php
@@ -1,8 +1,18 @@
 @section('script')
+    <script type="text/javascript" src="https://app.sandbox.midtrans.com/snap/snap.js"
+        data-client-key="{{ env('MIDTRANS_CLIENT_KEY') }}"></script>
+    <script src="https://cdn.jsdelivr.net/npm/sweetalert2@11"></script>
     <script type="text/javascript">
         $(document).ready(function() {
             $('#form').on('submit', function(e) {
                 e.preventDefault();
+                if ($("input:checkbox[name=tema]:checked").length <= 0) {
+                    Swal.fire({
+                        title: "Oops",
+                        text: "Tema Harus Dipilih Minimal 1",
+                        icon: "warning"
+                    });
+                }
                 let nama_lengkap = $('input[name="nama-lengkap"]').val();
                 let nama_panggilan = $('input[name="nama-panggilan"]').val();
                 let usia_saat_ini = $('input[name="usia-saat-ini"]').val();
@@ -14,20 +24,150 @@
                 $("input:checkbox[name=tema]:checked").each(function() {
                     tema.push($(this).val());
                 });
-                console.log({
-                    nama_lengkap,
-                    nama_panggilan,
-                    usia_saat_ini,
-                    tanggal_lahir,
-                    kelas,
-                    kategori,
-                    jadwal,
-                    tema
+
+                $.ajaxSetup({
+                    headers: {
+                        'X-CSRF-TOKEN': $('meta[name="csrf-token"]').attr('content')
+                    }
                 });
+
+                $.ajax({
+                    url: "{{ route('booking.kids.calculate') }}",
+                    type: "POST",
+                    data: {
+                        tema
+                    },
+                    success: function(data) {
+                        Swal.fire({
+                            title: "Konfirmasi Pendaftaran",
+                            showCancelButton: true,
+                            confirmButtonText: "Daftar",
+                            denyButtonText: `Kembali`,
+                            allowOutsideClick: false,
+                            allowEscapeKey: false,
+                            html: `
+                            <table class="text-start">
+                        <tr>
+                            <td>Nama Lengkap Anak</td>
+                            <td>:</td>
+                            <td id="nama-lengkap">${nama_lengkap}</td>
+                        </tr>
+                        <tr>
+                            <td>Nama Panggilan Anak</td>
+                            <td>:</td>
+                            <td id="nama-panggilan">${nama_panggilan}</td>
+                        </tr>
+                        <tr>
+                            <td>Tanggal Lahir</td>
+                            <td>:</td>
+                            <td id="tgl-lahir">${tanggal_lahir}</td>
+                        </tr>
+                        <tr>
+                            <td>Umur Saat ini</td>
+                            <td>:</td>
+                            <td id="umur">${usia_saat_ini}</td>
+                        </tr>
+                        <tr>
+                            <td>Kelas</td>
+                            <td>:</td>
+                            <td id="kelas">${$(`select[name="kelas_id"] option[value="${kelas}"]`)
+                            .html()}</td>
+                        </tr>
+                        <tr>
+                            <td>Kategori</td>
+                            <td>:</td>
+                            <td id="kategori">${$(
+                                `select[name="kategori_id"] option[value="${kategori}"]`)
+                            .html()}</td>
+                        </tr>
+                        <tr>
+                            <td>Jadwal</td>
+                            <td>:</td>
+                            <td id="jadwal">${$(
+                                `select[name="jadwal_id"] option[value="${jadwal}"]`)
+                            .html()}</td>
+                        </tr>
+                        <tr>
+                            <td>Tema yang Dipilih</td>
+                            <td>:</td>
+                            <td id="tema">
+                                <ul class="m-0" style="list-style-type: '- '; padding-left: 1.2em;">${ data.tema.map(value => `
+                                                                                <li>
+                                                                                    <div>
+                                                                                        <input type="hidden" name="tema[]" value="${value.id}" />
+                                                                                        <span>${value.nama}</span>
+                                                                                    </div>
+                                                                                </li>
+                                                                                `).join('')}
+                                </ul>
+                            </td>
+                        </tr>
+                        <tr>
+                            <td>Harga</td>
+                            <td>:</td>
+                            <td><b>${data.harga}</b></td>
+                        </tr>
+                        <tr>
+                            <td>Diskon</td>
+                            <td>:</td>
+                            <td><b>${data.diskon}</b></td>
+                        </tr>
+                        <tr>
+                            <td>Total Pembayaran</td>
+                            <td>:</td>
+                            <td><b>${data.total_bayar}</b></td>
+                        </tr>
+                    </table>
+                            `,
+                        }).then((result) => {
+                            if (result.isConfirmed) {
+                                $.ajax({
+                                    url: "{{ route('booking.kids.store') }}",
+                                    type: "POST",
+                                    data: {
+                                        nama_lengkap,
+                                        nama_panggilan,
+                                        usia_saat_ini,
+                                        tanggal_lahir,
+                                        kelas,
+                                        kategori,
+                                        jadwal,
+                                        tema
+                                    },
+                                    beforeSend: function() {
+                                        Swal.fire({
+                                            title: 'Memproses...',
+                                            text: 'Mohon tunggu sebentar',
+                                            allowOutsideClick: false,
+                                            didOpen: () => {
+                                                Swal
+                                                    .showLoading();
+                                            }
+                                        });
+                                    },
+                                    success: function(data) {
+                                        window.snap.pay(
+                                            `${data.snapToken}`);
+                                    },
+                                    error: function(xhr, status, error) {
+                                        Swal.fire({
+                                            icon: "error",
+                                            title: "Oops...",
+                                            text: xhr.responseJSON
+                                                .message,
+                                        });
+                                    },
+                                    complete: function() {
+                                        Swal.close();
+                                    }
+                                })
+                            }
+                        });
+                    },
+                })
             })
 
             $('select[name="kelas_id"]').on('change', function() {
-                console.log($(this).val());
                 let uriKategori = "{{ route('kategori.getdata', ['id' => ':id']) }}"
                     .replace(':id', $(this).val());
 
@@ -37,7 +177,6 @@
                     url: uriKategori,
                     type: "GET",
                     success: function(result) {
-                        console.log(result);
                         $('select[name="kategori_id"]').empty();
                         if (result.length == 0) {
                             $('select[name="kategori_id"]').append(`
@@ -59,14 +198,13 @@
                     url: uriTema,
                     type: "GET",
                     success: function(result) {
-                        console.log(result);
                         $('.tema-data').empty();
                         $('.tema-data').append(`<span>${result[0].nama}</span>`);
                         $('.tema-data').append(`<ul></ul>`);
                         $.each(result[0].detail_tema, function(key, val) {
                             $('.tema-data ul').append(`
                             <li>
-                                <input type="checkbox" id="${key}" name="tema" value="${val.id}" ${(key + 1) < mingguKeBerapa() ? 'disabled' : ''} />
+                                <input type="checkbox" id="${key}" name="tema" value="${val.id}" } ${(key + 1) < mingguKeBerapa() ? 'disabled' : '' />
                                 <label for="${key}">${val.nama} (Week ${val.week})</label>
                             </li>
                             `);
@@ -82,7 +220,6 @@
                     url: uriJadwal,
                     type: "GET",
                     success: function(result) {
-                        console.log(result);
                         $('select[name="jadwal_id"]').empty();
                         if (result.length == 0) {
                             $('select[name="jadwal_id"]').append(`
@@ -118,23 +255,23 @@
         <form action="" id="form">
             <div class="">
                 <label for="">Nama Lengkap Anak</label>
-                <input type="text" name="nama-lengkap">
+                <input type="text" name="nama-lengkap" required>
             </div>
             <div class="">
                 <label for="">Nama Panggilan Anak</label>
-                <input type="text" name="nama-panggilan">
+                <input type="text" name="nama-panggilan" required>
             </div>
             <div class="">
                 <label for="">Usia Anak Saat Ini</label>
-                <input type="number" name="usia-saat-ini">
+                <input type="number" name="usia-saat-ini" required>
             </div>
             <div class="">
                 <label for="">Tanggal Lahir anak</label>
-                <input type="date" name="tgl-lahir" id="">
+                <input type="date" name="tgl-lahir" id="" required>
             </div>
             <div class="">
                 <label for="">Pilih Kelas</label>
-                <select name="kelas_id" id="">
+                <select name="kelas_id" id="" required>
                     <option value="" selected disabled>== Pilih Kelas ==</option>
                     @foreach ($kids as $kelas)
                         <option value="{{ $kelas->id }}">{{ $kelas->nama }}</option>
@@ -143,13 +280,13 @@
             </div>
             <div class="">
                 <label for="">Pilih Kategori</label>
-                <select name="kategori_id" id="">
+                <select name="kategori_id" id="" required>
                     <option value="" selected disabled>== Pilih Kategori ==</option>
                 </select>
             </div>
             <div class="">
                 <label for="">Pilih Jadwal</label>
-                <select name="jadwal_id" id="">
+                <select name="jadwal_id" id="" required>
                     <option value="" selected disabled>== Pilih Jadwal ==</option>
                 </select>
             </div>
