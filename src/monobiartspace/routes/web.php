88747e0c518d8d8a170350391d862e954c1847c8
furqonaugust17 <furqonaugustseventeenth@gmail.com>
2025-05-30 14:46:36 +0700

    testing routing

--- src/monobiartspace/routes/web.php
+++ src/monobiartspace/routes/web.php
@@ -1,7 +1,8 @@
 <?php
 
-use App\Http\Controllers\MainController;
-use App\Http\Controllers\PendaftaranController;
+use App\Http\Controllers\Frontend\MainController;
+use App\Http\Controllers\Frontend\PaymentController;
+use App\Http\Controllers\Frontend\PendaftaranController;
 use App\Http\Controllers\productController;
 use App\Http\Controllers\ProfileController;
 use Illuminate\Support\Facades\Route;
@@ -16,11 +17,17 @@ Route::group(['prefix' => 'backend', 'middleware' => ['auth', 'verified', 'ifAdm
 });
 
 Route::get('product/{artSpace}', [productController::class, 'index'])->name('product.index');
+Route::get('email', [PendaftaranController::class, 'sendMail']);
+Route::get('data', [PaymentController::class, 'testing']);
 
 Route::middleware(['auth', 'verified'])->group(function () {
     Route::get('booking/artspace', [PendaftaranController::class, 'artSpace'])->name('booking.artspace');
     Route::post('booking/artspace', [PendaftaranController::class, 'storeArtSpace'])->name('booking.artspace.store');
+    Route::post('booking/artspace/calculate', [PendaftaranController::class, 'calculateArtSpaceTransaction'])->name('booking.artspace.calculate');
+
     Route::get('booking/kids', [PendaftaranController::class, 'kids'])->name('booking.kids');
+    Route::post('booking/kids', [PendaftaranController::class, 'storeKids'])->name('booking.kids.store');
+    Route::post('booking/kids/calculate', [PendaftaranController::class, 'calculateKidTransaction'])->name('booking.kids.calculate');
 });
 
 Route::middleware('auth')->group(function () {
