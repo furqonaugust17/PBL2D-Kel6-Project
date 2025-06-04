24e8acc64d0ae58d184a73cdc47ff83d2e861675
furqonaugust17 <furqonaugustseventeenth@gmail.com>
2025-06-05 00:03:09 +0700

    feat: create booking link

--- src/monobiartspace/resources/views/layouts/header.blade.php
+++ src/monobiartspace/resources/views/layouts/header.blade.php
@@ -18,6 +18,11 @@
                     <ul>
                         <li><a href="{{ route('booking.artspace') }}">Monobi Art Space</a></li>
                         <li><a href="{{ route('booking.kids') }}">Monobi Kids</a></li>
+                        @if (Auth::user())
+                            <li>
+                                <a href="{{ route('booking') }}">List Booking</a>
+                            </li>
+                        @endif
                     </ul>
                 </li>
             </ul>
