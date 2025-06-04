b53acb2703d054657d6a04614267b3abab5e8d7b
furqonaugust17 <furqonaugustseventeenth@gmail.com>
2025-06-05 00:04:16 +0700

    feat: change import paymentcontroller path

--- src/monobiartspace/routes/api.php
+++ src/monobiartspace/routes/api.php
@@ -2,14 +2,10 @@
 
 use App\Http\Controllers\JadwalKidController;
 use App\Http\Controllers\KategoriKidController;
-use App\Http\Controllers\PaymentController;
+use App\Http\Controllers\Frontend\PaymentController;
 use App\Http\Controllers\TemaKidController;
-use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
-// Route::get('/user', function (Request $request) {
-//     return $request->user();
-// })->middleware('auth:sanctum');
 Route::prefix('kids')->group(function () {
     Route::get('/kategori/{id}', [KategoriKidController::class, 'getData'])->name('kategori.getdata');
     Route::get('/tema/{id}', [TemaKidController::class, 'getData'])->name('tema.getdata');
