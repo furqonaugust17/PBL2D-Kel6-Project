ef85d0ef4732a914e6a6d389a737bb18cf7b4977
furqonaugust17 <furqonaugustseventeenth@gmail.com>
2025-06-08 14:43:16 +0700

    feat: change routing

--- src/monobiartspace/resources/views/layouts/header.blade.php
+++ src/monobiartspace/resources/views/layouts/header.blade.php
@@ -8,14 +8,15 @@
 
         <nav id="navmenu" class="navmenu">
             <ul>
-                <li><a href="#hero">Home</a></li>
-                <li><a href="#about">About</a></li>
-                <li><a href="#features">Features</a></li>
-                <li><a href="#services">Services</a></li>
-                <li><a href="#contact">Contact</a></li>
+                <li><a href="{{ Request::path() != '/' ? url('/#hero') : '#hero' }}">Home</a></li>
+                <li><a href="{{ Request::path() != '/' ? url('/#about') : '#about' }}">About</a></li>
+                <li><a href="{{ Request::path() != '/' ? url('/#class') : '#class' }}">Class</a></li>
+                <li><a href="{{ Request::path() != '/' ? url('/#services') : '#services' }}">Services</a></li>
+                <li><a href="{{ Request::path() != '/' ? url('/#contact') : '#contact' }}">Contact</a></li>
                 <li class="dropdown"><a href="#"><span>Booking</span>
                         <i class="bi bi-chevron-down toggle-dropdown"></i></a>
                     <ul>
+                        <li><a href="{{ route('class.index') }}">List Class</a></li>
                         <li><a href="{{ route('booking.artspace') }}">Monobi Art Space</a></li>
                         <li><a href="{{ route('booking.kids') }}">Monobi Kids</a></li>
                         @if (Auth::user())
