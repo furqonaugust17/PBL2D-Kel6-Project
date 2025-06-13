8e08d9fe450cb003f2e7386503eba41bf73b76ce
furqonaugust17 <furqonaugustseventeenth@gmail.com>
2025-06-13 13:42:33 +0700

    update: change booking dropdown list

--- src/monobiartspace/resources/views/layouts/header.blade.php
+++ src/monobiartspace/resources/views/layouts/header.blade.php
@@ -16,9 +16,9 @@
                 <li class="dropdown"><a href="#"><span>Booking</span>
                         <i class="bi bi-chevron-down toggle-dropdown"></i></a>
                     <ul>
-                        <li><a href="{{ route('class.index') }}">List Class</a></li>
                         <li><a href="{{ route('booking.artspace') }}">Monobi Art Space</a></li>
                         <li><a href="{{ route('booking.kids') }}">Monobi Kids</a></li>
+                        <li><a href="{{ route('class.index') }}">List Class</a></li>
                         @if (Auth::user())
                             <li>
                                 <a href="{{ route('booking') }}">List Booking</a>
