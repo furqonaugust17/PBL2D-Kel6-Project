3057d86f5325d700dd6ae7db85b6eab7228ba969
furqonaugust17 <furqonaugustseventeenth@gmail.com>
2025-06-13 13:40:04 +0700

    feat: create relationship function

--- src/monobiartspace/app/Models/KategoriKid.php
+++ src/monobiartspace/app/Models/KategoriKid.php
@@ -5,6 +5,7 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Relations\BelongsTo;
+use Illuminate\Database\Eloquent\Relations\HasMany;
 use Illuminate\Database\Eloquent\SoftDeletes;
 
 class KategoriKid extends Model
@@ -16,5 +17,10 @@ class KategoriKid extends Model
     public function kid(): BelongsTo
     {
         return $this->belongsTo(Kid::class);
+    }
+
+    public function jadwal(): HasMany
+    {
+        return $this->hasMany(JadwalKid::class, 'kategori_id');
     }
 }
