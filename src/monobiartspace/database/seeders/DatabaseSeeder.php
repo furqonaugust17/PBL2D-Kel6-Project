7cfecec2dca99ac57c02af2064479c8976b49d01
furqonaugust17 <furqonaugustseventeenth@gmail.com>
2025-06-14 09:21:04 +0700

    test: change call seeder

--- src/monobiartspace/database/seeders/DatabaseSeeder.php
+++ src/monobiartspace/database/seeders/DatabaseSeeder.php
@@ -17,7 +17,7 @@ class DatabaseSeeder extends Seeder
         // User::factory(10)->create();
         $this->call([ArtSpaceSeeder::class, KidSeeder::class, RolesAndPermissionsSeeder::class]);
 
-        $this->call([RolesAndPermissionsSeeder::class, KaryawanSeeder::class]);
+        // $this->call([RolesAndPermissionsSeeder::class, KaryawanSeeder::class]);
 
         $superadmin = User::create([
             'name' => 'superadmin',
