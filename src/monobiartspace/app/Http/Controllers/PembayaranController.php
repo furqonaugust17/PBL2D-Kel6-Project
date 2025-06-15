a2e4c2aec5aa8881cea8eca71b745a6650610331
furqonaugust17 <furqonaugustseventeenth@gmail.com>
2025-06-15 20:44:03 +0700

    feat: create index and show method

---
+++ src/monobiartspace/app/Http/Controllers/PembayaranController.php
@@ -0,0 +1,85 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\PembayaranBooking;
+use App\Services\PembayaranService;
+use Illuminate\Http\Request;
+use Yajra\DataTables\Facades\DataTables;
+
+class PembayaranController extends Controller
+{
+
+    protected $pembayaranService;
+    public function __construct(PembayaranService $pembayaranService)
+    {
+        $this->pembayaranService = $pembayaranService;
+    }
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        if (request()->ajax()) {
+            $pembayaran = PembayaranBooking::with(['pendaftaran', 'pendaftaran.customer'])->latest();
+            return DataTables::of($pembayaran)->make();
+        }
+        return view('backend.pembayaran.index');
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(PembayaranBooking $pembayaran, String $type)
+    {
+        // if ($type == 'kids') {
+        //     $data = (object) $this->pembayaranService->getDataPembayaranKids($pembayaran->order_id);
+        //     return view('backend.pembayaran.kid', compact('data'));
+        // } else {
+        //     $data = (object) $this->pembayaranService->getDataPembayaranArtSpace($pembayaran->order_id);
+        //     return view('backend.pembayaran.artspace', compact('data'));
+        // }
+        $data = $pembayaran->load(['pendaftaran.customer.user']);
+        return view('backend.pembayaran.detail', compact('data'));
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     */
+    public function edit(PembayaranBooking $pembayaran)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(Request $request, PembayaranBooking $pembayaran)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(PembayaranBooking $pembayaran)
+    {
+        //
+    }
+}
