f7693f9ff78e7d3154c7559abdb5902583dbe9f4
furqonaugust17 <furqonaugustseventeenth@gmail.com>
2025-06-18 11:17:46 +0700

    feat: create pendaftaran route

--- src/monobiartspace/routes/web.php
+++ src/monobiartspace/routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\PendaftaranController;
 use App\Http\Controllers\ProfileController;
 use Illuminate\Support\Facades\Route;
 
@@ -12,6 +13,8 @@ Route::group(['prefix' => 'backend', 'middleware' => ['auth', 'verified']], func
     Route::get('/dashboard', function () {
         return view('dashboard');
     })->name('dashboard');
+
+    Route::resource('pendaftaran', PendaftaranController::class);
 });
 
 Route::middleware('auth')->group(function () {
