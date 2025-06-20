620aaa9df4a58a9175a8b0e726bec4f34820c4ae
furqonaugust17 <furqonaugustseventeenth@gmail.com>
2025-06-20 07:52:18 +0700

    feat: add customer menu

--- src/monobiartspace/resources/views/backend/layouts/sidebar.blade.php
+++ src/monobiartspace/resources/views/backend/layouts/sidebar.blade.php
@@ -15,6 +15,11 @@
                     <span class="nav-text">Dashboard</span>
                 </a>
             </li>
+            <li><a class="ai-icon" href="{{ route('customer.index') }}">
+                    <i class="fas fa-user"></i>
+                    <span class="nav-text">Customer</span>
+                </a>
+            </li>
         </ul>
     </div>
 </div>
