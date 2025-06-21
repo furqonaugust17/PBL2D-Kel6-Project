b07f69a917f931e5622944910d43b3bbc77e3407
furqonaugust17 <furqonaugustseventeenth@gmail.com>
2025-06-21 23:44:29 +0700

    feat: create galeri route

--- src/monobiartspace/routes/web.php
+++ src/monobiartspace/routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\GaleriController;
 use App\Http\Controllers\ProfileController;
 use Illuminate\Support\Facades\Route;
 
@@ -12,6 +13,8 @@ Route::group(['prefix' => 'backend', 'middleware' => ['auth', 'verified']], func
     Route::get('/dashboard', function () {
         return view('dashboard');
     })->name('dashboard');
+
+    Route::resource('galeri', GaleriController::class);
 });
 
 Route::middleware('auth')->group(function () {
