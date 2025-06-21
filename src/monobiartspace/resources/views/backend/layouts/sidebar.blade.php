0418c90fb1ba86a0f163dde35f84da5b700951d8
furqonaugust17 <furqonaugustseventeenth@gmail.com>
2025-06-21 23:44:10 +0700

    feat: create galeri menu

--- src/monobiartspace/resources/views/backend/layouts/sidebar.blade.php
+++ src/monobiartspace/resources/views/backend/layouts/sidebar.blade.php
@@ -15,6 +15,11 @@
                     <span class="nav-text">Dashboard</span>
                 </a>
             </li>
+            <li><a class="ai-icon" href="{{ route('galeri.index') }}">
+                    <i class="fas fa-images"></i>
+                    <span class="nav-text">Galeri</span>
+                </a>
+            </li>
         </ul>
     </div>
 </div>
