43dc046e9712919036a6217a08211935d726b507
furqonaugust17 <furqonaugustseventeenth@gmail.com>
2025-06-21 23:43:42 +0700

    feat: create delete function for single image

--- src/monobiartspace/app/Services/ImageUploadService.php
+++ src/monobiartspace/app/Services/ImageUploadService.php
@@ -40,4 +40,9 @@ class ImageUploadService
             $image->delete();
         }
     }
+
+    public function deleteSingleImage($image)
+    {
+        Storage::disk('public')->delete($image);
+    }
 }
