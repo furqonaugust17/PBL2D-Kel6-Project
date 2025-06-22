805cd20c343ed19cc3452e4145bb602a49d59a8c
furqonaugust17 <furqonaugustseventeenth@gmail.com>
2025-06-22 09:48:38 +0700

    update: call RoleAndPermissionsSeeder

--- src/monobiartspace/database/seeders/DatabaseSeeder.php
+++ src/monobiartspace/database/seeders/DatabaseSeeder.php
@@ -15,7 +15,7 @@ class DatabaseSeeder extends Seeder
     public function run(): void
     {
         // User::factory(10)->create();
-
+        $this->call([RolesAndPermissionsSeeder::class]);
         User::factory()->create([
             'name' => 'superadmin',
             'email' => 'superadmin@gmail.com',
