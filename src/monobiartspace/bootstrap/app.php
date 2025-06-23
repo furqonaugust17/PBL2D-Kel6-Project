13248f37ec7e1b96d081943467fbb10aec4780e6
furqonaugust17 <furqonaugustseventeenth@gmail.com>
2025-06-23 21:49:19 +0700

    fix: fix duplicate middleware alias

--- src/monobiartspace/bootstrap/app.php
+++ src/monobiartspace/bootstrap/app.php
@@ -18,12 +18,9 @@ return Application::configure(basePath: dirname(__DIR__))
         $middleware->alias([
             'role' => \Spatie\Permission\Middleware\RoleMiddleware::class,
             'ifAdmin'   => CheckIfAdmin::class,
+            'ExceptSupervisor' => ExceptSupervisor::class,
             'permission' => \Spatie\Permission\Middleware\PermissionMiddleware::class,
             'role_or_permission' => \Spatie\Permission\Middleware\RoleOrPermissionMiddleware::class,
-        ]);
-
-        $middleware->alias([
-            'ExceptSupervisor' => ExceptSupervisor::class
         ]);
     })
     ->withExceptions(function (Exceptions $exceptions) {
