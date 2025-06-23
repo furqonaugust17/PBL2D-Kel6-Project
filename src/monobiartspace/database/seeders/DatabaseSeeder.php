d11ea5b721525d5044055ddf83e639070a0e90fb
furqonaugust17 <furqonaugustseventeenth@gmail.com>
2025-06-23 08:51:41 +0700

    feat: add jadwal art space seeder

--- src/monobiartspace/database/seeders/DatabaseSeeder.php
+++ src/monobiartspace/database/seeders/DatabaseSeeder.php
@@ -15,7 +15,7 @@ class DatabaseSeeder extends Seeder
     public function run(): void
     {
         // User::factory(10)->create();
-        $this->call([ArtSpaceSeeder::class, KidSeeder::class, RolesAndPermissionsSeeder::class]);
+        $this->call([ArtSpaceSeeder::class, KidSeeder::class, JadwalArtSpaceSeeder::class, RolesAndPermissionsSeeder::class]);
 
         // $this->call([RolesAndPermissionsSeeder::class, KaryawanSeeder::class]);
 
