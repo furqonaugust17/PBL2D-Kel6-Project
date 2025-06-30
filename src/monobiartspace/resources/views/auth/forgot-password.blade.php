3224f81f5f8a73675bb15c88f26ed303bdec4a0a
furqonaugust17 <furqonaugustseventeenth@gmail.com>
2025-07-01 01:35:31 +0700

    update: change new layout

--- src/monobiartspace/resources/views/auth/forgot-password.blade.php
+++ src/monobiartspace/resources/views/auth/forgot-password.blade.php
@@ -1,27 +1,48 @@
 <x-guest-layout>
     <x-slot:title>Lupa Password</x-slot:title>
-    <div class="mb-4 text-sm text-gray-600 dark:text-gray-400">
-        {{ __('Forgot your password? No problem. Just let us know your email address and we will email you a password reset link that will allow you to choose a new one.') }}
+    <div class="authincation h-100">
+        <div class="container h-100">
+            <div class="row justify-content-center h-100 align-items-center">
+                <div class="col-md-6">
+                    <div class="authincation-content">
+                        <div class="row no-gutters">
+                            <div class="auth-form">
+                                <div class="text-center mb-3">
+                                    <img src="{{ asset('images/monobi_logo.png') }}" width="50%" alt="">
+                                </div>
+                                <p class="text-center text-body-secondary mb-4">
+                                    {{ __('Lupa kata sandi kamu? Tenang aja! Masukkan email kamu dan kami akan kirimkan link buat atur ulang kata sandinya.') }}
+                                </p>
+                                <!-- Session Status -->
+                                <x-auth-session-status class="mb-4" :status="session('status')" />
+
+                                <form method="POST" action="{{ route('password.email') }}">
+                                    @csrf
+
+                                    <!-- Email Address -->
+                                    <div class="form-group">
+                                        <x-input-label for="email" class="mb-1">
+                                            <strong>Email</strong>
+                                        </x-input-label>
+                                        <x-text-input id="email" class="form-control mt-1 w-full" type="email"
+                                            name="email" :value="old('email')" required autofocus />
+                                        @error('email')
+                                            <div class="invalid-feedback">
+                                                {{ $message }}
+                                            </div>
+                                        @enderror
+                                    </div>
+
+                                    <div class="text-center">
+                                        <button type="submit" class="btn btn-primary btn-block">Email Password Reset
+                                            Link</button>
+                                    </div>
+                                </form>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
     </div>
-
-    <!-- Session Status -->
-    <x-auth-session-status class="mb-4" :status="session('status')" />
-
-    <form method="POST" action="{{ route('password.email') }}">
-        @csrf
-
-        <!-- Email Address -->
-        <div>
-            <x-input-label for="email" :value="__('Email')" />
-            <x-text-input id="email" class="block mt-1 w-full" type="email" name="email" :value="old('email')" required
-                autofocus />
-            <x-input-error :messages="$errors->get('email')" class="mt-2" />
-        </div>
-
-        <div class="flex items-center justify-end mt-4">
-            <x-primary-button>
-                {{ __('Email Password Reset Link') }}
-            </x-primary-button>
-        </div>
-    </form>
 </x-guest-layout>
--- src/monobiartspace/resources/views/auth/reset-password.blade.php
+++ src/monobiartspace/resources/views/auth/reset-password.blade.php
@@ -1,41 +1,75 @@
 <x-guest-layout>
     <x-slot:title>Reset Password</x-slot:title>
-    <form method="POST" action="{{ route('password.store') }}">
-        @csrf
+    <div class="authincation h-100">
+        <div class="container h-100">
+            <div class="row justify-content-center h-100 align-items-center">
+                <div class="col-md-6">
+                    <div class="authincation-content">
+                        <div class="row no-gutters">
+                            <div class="auth-form">
+                                <div class="text-center mb-3">
+                                    <img src="{{ asset('images/monobi_logo.png') }}" width="50%" alt="">
+                                </div>
+                                <p class="text-center text-body-secondary mb-4">
+                                    {{ __('Yuk, atur ulang password kamu di sini. Biar bisa login lagi tanpa drama!') }}
+                                </p>
+                                <form method="POST" action="{{ route('password.store') }}">
+                                    @csrf
 
-        <!-- Password Reset Token -->
-        <input type="hidden" name="token" value="{{ $request->route('token') }}">
+                                    <!-- Password Reset Token -->
+                                    <input type="hidden" name="token" value="{{ $request->route('token') }}">
 
-        <!-- Email Address -->
-        <div>
-            <x-input-label for="email" :value="__('Email')" />
-            <x-text-input id="email" class="block mt-1 w-full" type="email" name="email" :value="old('email', $request->email)" required
-                autofocus autocomplete="username" />
-            <x-input-error :messages="$errors->get('email')" class="mt-2" />
+                                    <!-- Email Address -->
+                                    <div class="form-group">
+                                        <x-input-label for="email" class="mb-1">
+                                            <strong>Email</strong>
+                                        </x-input-label>
+                                        <x-text-input id="email" class="form-control" type="email" name="email"
+                                            :value="old('email', $request->email)" required autofocus autocomplete="username" />
+                                        @error('email')
+                                            <div class="invalid-feedback">
+                                                {{ $message }}
+                                            </div>
+                                        @enderror
+                                    </div>
+
+                                    <!-- Password -->
+                                    <div class="form-group">
+                                        <x-input-label for="password" class="mb-1">
+                                            <strong>Password</strong>
+                                        </x-input-label>
+                                        <x-text-input id="password" class="form-control" type="password"
+                                            name="password" required autocomplete="new-password" />
+                                        @error('password')
+                                            <div class="invalid-feedback">
+                                                {{ $message }}
+                                            </div>
+                                        @enderror
+                                    </div>
+
+                                    <!-- Confirm Password -->
+                                    <div class="form-group">
+                                        <x-input-label for="password" class="mb-1">
+                                            <strong>Konfirmasi Password</strong>
+                                        </x-input-label>
+
+                                        <x-input-error :messages="$errors->get('password_confirmation')" class="mt-2" />
+                                        @error('password_confirmation')
+                                            <div class="invalid-feedback">
+                                                {{ $message }}
+                                            </div>
+                                        @enderror
+                                    </div>
+
+                                    <div class="text-center">
+                                        <button type="submit" class="btn btn-primary btn-block">Reset Password</button>
+                                    </div>
+                                </form>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
         </div>
-
-        <!-- Password -->
-        <div class="mt-4">
-            <x-input-label for="password" :value="__('Password')" />
-            <x-text-input id="password" class="block mt-1 w-full" type="password" name="password" required
-                autocomplete="new-password" />
-            <x-input-error :messages="$errors->get('password')" class="mt-2" />
-        </div>
-
-        <!-- Confirm Password -->
-        <div class="mt-4">
-            <x-input-label for="password_confirmation" :value="__('Confirm Password')" />
-
-            <x-text-input id="password_confirmation" class="block mt-1 w-full" type="password"
-                name="password_confirmation" required autocomplete="new-password" />
-
-            <x-input-error :messages="$errors->get('password_confirmation')" class="mt-2" />
-        </div>
-
-        <div class="flex items-center justify-end mt-4">
-            <x-primary-button>
-                {{ __('Reset Password') }}
-            </x-primary-button>
-        </div>
-    </form>
+    </div>
 </x-guest-layout>
