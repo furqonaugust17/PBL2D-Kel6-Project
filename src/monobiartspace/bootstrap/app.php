12777fae4933ae4d69d4dfa85e8092a861853d06
furqonaugust17 <furqonaugustseventeenth@gmail.com>
2025-07-03 00:29:18 +0700

    fix: add trusted

--- src/monobiartspace/bootstrap/app.php
+++ src/monobiartspace/bootstrap/app.php
@@ -24,6 +24,7 @@ return Application::configure(basePath: dirname(__DIR__))
             'role_or_permission' => \Spatie\Permission\Middleware\RoleOrPermissionMiddleware::class,
             'inventaris' => InventarisMiddleware::class
         ]);
+        $middleware->trustProxies(at: '*');
     })
     ->withExceptions(function (Exceptions $exceptions) {
         //
