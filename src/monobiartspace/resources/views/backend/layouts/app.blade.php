45bed46f1e1afed1bd705a105c0c365809c14036
furqonaugust17 <furqonaugustseventeenth@gmail.com>
2025-07-08 02:46:34 +0700

    fix: fix favicon

--- src/monobiartspace/resources/views/backend/layouts/app.blade.php
+++ src/monobiartspace/resources/views/backend/layouts/app.blade.php
@@ -12,12 +12,11 @@
     <meta name="description" content="MonobiArtSpace" />
     <meta property="og:title" content="MonobiArtSpace" />
     <meta property="og:description" content="MonobiArtSpace" />
-    <meta property="og:image" content="https://zenix.dexignzone.com/xhtml/social-image.png" />
     <meta name="format-detection" content="telephone=no">
     <meta name="csrf-token" content="{{ csrf_token() }}">
     <title>{{ $title }}</title>
     <!-- Favicon icon -->
-    <link rel="icon" type="image/png" sizes="16x16" href="{{ asset('images/favicon.png">') }}" />
+    <link rel="icon" type="image/png" sizes="16x16" href="{{ asset('images/monobi_icon.png') }}" />
     @yield('css')
     <link href="{{ asset('plugins/vendor/bootstrap-select/dist/css/bootstrap-select.min.css') }}" rel="stylesheet">
     <link href="{{ asset('plugins/vendor/owl-carousel/owl.carousel.css') }}" rel="stylesheet">
