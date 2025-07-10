c012fed30d5cf4e1e4d449bb5fb3ee70d154c92b
furqonaugust17 <furqonaugustseventeenth@gmail.com>
2025-07-11 00:42:11 +0700

    update: change title

--- src/monobiartspace/resources/views/backend/galeri/edit.blade.php
+++ src/monobiartspace/resources/views/backend/galeri/edit.blade.php
@@ -19,7 +19,7 @@
     </script>
 @endsection
 <x-app-layout>
-    <x-slot:title>Tambah Galeri</x-slot:title>
+    <x-slot:title>Edit Galeri</x-slot:title>
     <div class="row">
         <div class="col-12">
             <div class="card">
--- src/monobiartspace/resources/views/backend/kids/harga/edit.blade.php
+++ src/monobiartspace/resources/views/backend/kids/harga/edit.blade.php
@@ -1,5 +1,5 @@
 <x-app-layout>
-    <x-slot:title>Tambah Harga Monobi Kids</x-slot:title>
+    <x-slot:title>Edit Harga Monobi Kids</x-slot:title>
     <div class="row">
         <div class="col-12">
             <div class="card">
